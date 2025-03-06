817d267725c00db22cfd8759e6535b3843406b17
Bench-10 <145767536+Bench-10@users.noreply.github.com>
2025-03-06 23:00:08 +0800

    removed git practice

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,7 @@
 import { timerFormats } from './timerData.js';
 import { generate } from './timerFunction.js';
 
-console.log("gittesting");
-console.log("learning clone and pusjhon git hub");
+//I removed the git practice code from here
 
 let timerFormatingHTML = '';
 
